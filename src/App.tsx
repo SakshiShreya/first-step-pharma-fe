045138d68a2d43798c1f2f79e4331982dd5fd832
SakshiShreya <shreyasakshi96@gmail.com>
2021-11-07 01:39:04 +0530

    Add eslint and prettierrc files for linting and code-formatting

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-import { HashRouter } from 'react-router-dom';
+import { HashRouter } from "react-router-dom";
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <HashRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    </HashRouter>
-  );
-}
+interface AppProps {}
+
+const App: React.FunctionComponent<AppProps> = () => (
+  <HashRouter>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit
+          <code>src/App.tsx</code>
+          and save to reload.
+        </p>
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+          Learn React
+        </a>
+      </header>
+    </div>
+  </HashRouter>
+);
 
 export default App;
