b92440c14123b5eba81a618fd659ddece41bfee8
SakshiShreya <shreyasakshi96@gmail.com>
2021-11-07 02:48:25 +0530

    Add theme file and test class

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-import { HashRouter } from 'react-router-dom';
+import "./App.scss";
+import { HashRouter } from "react-router-dom";
 
 function App() {
   return (
     <HashRouter>
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <div className="test">abc</div>
     </HashRouter>
   );
 }
