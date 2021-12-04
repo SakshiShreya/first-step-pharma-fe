2b6673fb99feebe0d08b15f5f422218e871f4c9b
SakshiShreya <shreyasakshi96@gmail.com>
2021-12-05 00:45:42 +0530

    Add loader instead of hiding footer
    Also change theme file to partial

---
+++ src/components/generic/loader/loader.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import styles from "./loader.module.scss";
+
+interface LoaderProps {
+  width: number | string;
+  height: number | string;
+  style?: React.CSSProperties;
+}
+
+const Loader: React.FunctionComponent<LoaderProps> = ({
+  height,
+  width,
+  style,
+}) => <span className={styles.loader} style={{ height, width, ...style }} />;
+
+Loader.defaultProps = {
+  style: {},
+};
+
+export default Loader;
