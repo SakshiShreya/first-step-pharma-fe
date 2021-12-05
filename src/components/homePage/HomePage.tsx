dbf24a0302f444a0870dc44a31667a53a7145fda
SakshiShreya <shreyasakshi96@gmail.com>
2021-12-05 17:18:34 +0530

    Separate Cover component from homepage

--- src/components/homePage/HomePage.tsx
+++ src/components/homePage/HomePage.tsx
@@ -1,35 +1,12 @@
-// import axios from "axios";
 import * as React from "react";
-import useWindowDimensions from "../../hooks/useWindowDimensions";
-import styles from "./HomePage.module.scss";
+import Cover from "./cover/Cover";
 
 interface HomePageProps {}
 
-const HomePage: React.FunctionComponent<HomePageProps> = () => {
-  const dimensions = useWindowDimensions();
-  const desktopStyle = {
-    backgroundImage: `url("${process.env.PUBLIC_URL}/images/placeholder-1024.png")`,
-  };
-
-  return (
-    <div
-      className={styles.cont}
-      style={dimensions.device === "desktop" ? desktopStyle : {}}
-    >
-      {dimensions.device !== "desktop" && (
-        <div className={styles.image}>
-          <img
-            src={`${process.env.PUBLIC_URL}/images/placeholder-1024.png`}
-            alt=""
-          />
-        </div>
-      )}
-      <div className={`${styles.text} container`}>
-        <h1 className={styles.h1}>First Step Pharma</h1>
-        <h2 className={`${styles.h2} roboto`}>Pharmaceutical Consultancy</h2>
-      </div>
-    </div>
-  );
-};
+const HomePage: React.FunctionComponent<HomePageProps> = () => (
+  <div>
+    <Cover />
+  </div>
+);
 
 export default HomePage;
