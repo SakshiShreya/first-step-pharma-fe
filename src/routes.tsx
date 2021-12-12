f3203586bfa4736d25f88fda4048fc474c6350b6
krishna1212031 <krishna1212031@gmail.com>
2021-12-12 19:26:07 +0530

    404 page created,issue fixed #32

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,14 @@
 import * as React from "react";
 import { Route, Switch } from "react-router-dom";
+import Error404 from "./components/error/Error404";
 import HomePage from "./components/homePage/HomePage";
 
 interface RoutesProps {}
 
 const Routes: React.FunctionComponent<RoutesProps> = () => (
   <Switch>
-    <Route path="/">
-      <HomePage />
-    </Route>
+    <Route path="/" component={HomePage} exact />
+    <Route component={Error404} />
   </Switch>
 );
 
